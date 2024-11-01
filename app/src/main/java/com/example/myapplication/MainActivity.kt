d51232361da0a06c25fe5974bd6c54dccb8056d8
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-31 22:06:15 -0300

    Implementando remember na variavel

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,8 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -15,6 +18,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -30,7 +35,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     CounterScreen()
                 }
             }
@@ -41,17 +49,33 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun CounterScreen() {
+    var idade = remember {
+        mutableStateOf(0)
+    }
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(text = "Qual a sua idade?", fontSize = 24.sp, color =  Color(0xFFAD1F4E));
+        Text(text = "Qual a sua idade?", fontSize = 24.sp, color = Color(0xFFAD1F4E));
         Text(text = "Aperta os botões para informar a sua idade.", fontSize = 12.sp)
-        Text(text = "21", fontSize = 48.sp, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(32.dp))
+        Text(text = "${idade.value}", fontSize = 48.sp, fontWeight = FontWeight.Bold)
 
         Row() {
-            Button(onClick = {}, modifier = Modifier.size(84.dp), shape = RoundedCornerShape(8.dp), colors = ButtonDefaults.buttonColors(Color(0xFFAD1F4E))) { Text(text = "-", fontSize = 40.sp) }
-            Button(onClick = {}, modifier = Modifier.size(84.dp), shape = RoundedCornerShape(8.dp), colors = ButtonDefaults.buttonColors(Color(0xFFAD1F4E))) { Text(text = "+", fontSize = 40.sp) }
+            Button(
+                onClick = { idade.value-- },
+                modifier = Modifier.size(84.dp),
+                shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.buttonColors(Color(0xFFAD1F4E))
+            ) { Text(text = "-", fontSize = 40.sp) }
+            
+            Spacer(modifier = Modifier.width(32.dp))
+            Button(
+                onClick = { idade.value++ },
+                modifier = Modifier.size(84.dp),
+                shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.buttonColors(Color(0xFFAD1F4E))
+            ) { Text(text = "+", fontSize = 40.sp) }
         }
     }
 }
